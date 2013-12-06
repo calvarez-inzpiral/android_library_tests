fb68113fbb85e35f88396a14a8c6cad7fc92b2c7
Claudio Alvarez <claudio.alvarez@inzpiral.com>
2013-12-06 11:17:41 -0300

    se agrega chained choice y repetition

---
+++ FrogmiGsonTest/src/com/javacodegeeks/android/json/model/ChainedChoice.java
@@ -0,0 +1,15 @@
+package com.javacodegeeks.android.json.model;
+
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ChainedChoice extends FrogmiActivity {
+	
+	@SerializedName("question")
+	public String mQuestion;
+	
+	@SerializedName("chain_choice")
+	public List<Choice> mChildren;
+	
+}
--- FrogmiGsonTest/src/com/javacodegeeks/android/json/model/Picture.java
+++ FrogmiGsonTest/src/com/javacodegeeks/android/json/model/Picture.java
@@ -4,19 +4,10 @@ import com.google.gson.annotations.SerializedName;
 
 public class Picture extends FrogmiActivity {
 
-	
 	@SerializedName("question")
 	public String mQuestion;
-	
-	@SerializedName("value")
-	public String mValue;
 
 	@SerializedName("quality")
 	public int mQuality;
 	
-	@SerializedName("created_at")
-	public String mCreatedAt;
-	
-	@SerializedName("updated_at")
-	public String mUpdatedAt;
 }
