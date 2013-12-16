fe0d1192bd7f3d3bd2f71423d8a268e3b42f8e65
Juan Pablo Lazcano <jp.lazcano87@gmail.com>
2013-12-16 14:40:04 -0300

    picture modulle

--- Consumer/src/com/inzpiral/consumer/models/Picture.java
+++ Consumer/src/com/inzpiral/consumer/models/Picture.java
@@ -1,6 +1,15 @@
 package com.inzpiral.consumer.models;
 
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.google.gson.annotations.SerializedName;
+import com.inzpiral.consumer.R;
+
 
 public class Picture extends FrogmiActivity {
 
@@ -24,6 +33,40 @@ public class Picture extends FrogmiActivity {
 
 	public void setQuality(int mQuality) {
 		this.mQuality = mQuality;
+	}
+	
+	@Override
+	public void display(Activity activity, View ParentView, int parentId) {
+		mActivity = activity;
+		mParentView = ParentView;
+		mParentId = parentId;
+		
+		System.out.println("MOSTRANDOME! Soy un 'Boolean' de question: " + getQuestion());
+		PictureController controller = new PictureController(new PictureView());
+	}
+
+	
+	private class PictureController {
+		public PictureController(PictureView sectionView) {
+			((LinearLayout) mParentView.findViewById(mParentId)).addView(sectionView);
+			//sectionView.getPictureButton().setText(getQuestion());
+		}
+	}
+	
+	private class PictureView extends LinearLayout {
+		public PictureView() {
+			super(mActivity);
+			LayoutInflater mInflater = LayoutInflater.from(mActivity);
+			addView(mInflater.inflate(R.layout.module_picture, null));
+			getTextViewButton().setText(getQuestion());
+		}
+		
+		public Button getPictureButton() {
+			return (Button)findViewById(R.id.btn_picture);
+		}
+		public TextView getTextViewButton() {
+			return (TextView)findViewById(R.id.txt_PictureTitle);
+		}
 	}
 	
 }
