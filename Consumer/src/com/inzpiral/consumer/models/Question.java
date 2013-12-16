05ba88670ebb18b5dcf80d071f1ac108482305c5
Claudio Alvarez <claudio.alvarez@inzpiral.com>
2013-12-16 14:42:16 -0300

    view y controller de Questions

--- Consumer/src/com/inzpiral/consumer/models/Question.java
+++ Consumer/src/com/inzpiral/consumer/models/Question.java
@@ -1,6 +1,15 @@
 package com.inzpiral.consumer.models;
 
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.google.gson.annotations.SerializedName;
+import com.inzpiral.consumer.R;
 
 public class Question extends FrogmiActivity {
 
@@ -13,6 +22,40 @@ public class Question extends FrogmiActivity {
 
 	public void setQuestion(String mQuestion) {
 		this.mQuestion = mQuestion;
+	}
+	
+	@Override
+	public void display(Activity activity, View ParentView, int parentId) {
+		mActivity = activity;
+		mParentView = ParentView;
+		mParentId = parentId;
+		
+		System.out.println("MOSTRANDOME! Soy un 'Question' de question: " + getQuestion());
+		QuestionController controller = new QuestionController(new QuestionView());
+	}
+
+	
+	private class QuestionController {
+		public QuestionController(QuestionView questionView) {
+			((LinearLayout) mParentView.findViewById(mParentId)).addView(questionView);
+			questionView.getQuestionTextView().setText(getQuestion());
+		}
+	}
+	
+	private class QuestionView extends LinearLayout {
+		public QuestionView() {
+			super(mActivity);
+			LayoutInflater mInflater = LayoutInflater.from(mActivity);
+			addView(mInflater.inflate(R.layout.module_question, null));
+		}
+		
+		public TextView getQuestionTextView() {
+			return (TextView)findViewById(R.id.question);
+		}
+		
+		public EditText getFieldEditText() {
+			return (EditText)findViewById(R.id.field);
+		}
 	}
 	
 }
