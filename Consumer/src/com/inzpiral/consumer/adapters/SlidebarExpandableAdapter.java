1c8645df20aab3e9df56c09b1bb3c8bc618fed5c
Juan Pablo Lazcano <jp.lazcano87@gmail.com>
2013-12-30 14:21:57 -0300

    expandable adapter

--- Consumer/src/com/inzpiral/consumer/adapters/SlidebarExpandableAdapter.java
+++ Consumer/src/com/inzpiral/consumer/adapters/SlidebarExpandableAdapter.java
@@ -1,70 +1,111 @@
 package com.inzpiral.consumer.adapters;
 
+import java.util.ArrayList;
+
+import com.inzpiral.consumer.R;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SlidebarExpandableAdapter extends BaseExpandableListAdapter{
 
+	private Activity activity;
+	private ArrayList<Object> childtems;
+	private LayoutInflater inflater;
+	private ArrayList<String> parentItems, child;
+
+
+	public SlidebarExpandableAdapter(ArrayList<Object> childrens, ArrayList<String> parents) {
+		this.childtems = childrens;
+		this.parentItems = parents;
+
+	}
+
+	public void setInflater(LayoutInflater inflater, Activity activity) {
+		this.inflater = inflater;
+		this.activity = activity;
+	}
+
 	@Override
 	public Object getChild(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public long getChildId(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public View getChildView(int groupPosition, int childPosition,
-			boolean isLastChild, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+		child = (ArrayList<String>) childtems.get(groupPosition);
+
+		TextView textView = null;
+
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.group_expandable, null);
+		}
+
+		textView = (TextView) convertView.findViewById(R.id.textView1);
+		textView.setText(child.get(childPosition));
+
+		convertView.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(activity, child.get(childPosition),
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		return convertView;	}
 
 	@Override
 	public int getChildrenCount(int groupPosition) {
-		// TODO Auto-generated method stub
-		return 0;
+		return ((ArrayList<String>) childtems.get(groupPosition)).size();
 	}
 
 	@Override
 	public Object getGroup(int groupPosition) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int getGroupCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return parentItems.size();
 	}
 
 	@Override
 	public long getGroupId(int groupPosition) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public View getGroupView(int groupPosition, boolean isExpanded,
-			View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
+	public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.row_expandable, null);
+		}
+
+		((CheckedTextView) convertView).setText(parentItems.get(groupPosition));
+		((CheckedTextView) convertView).setChecked(isExpanded);
+
+		return convertView;
 	}
 
 	@Override
 	public boolean hasStableIds() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
